9abe4fa2d2d2022d8a273bde1095e8e1ea616903
Rajowan <110572622+RajowanRJ@users.noreply.github.com>
2023-11-19 23:27:24 +0600

    Adding all the Problem solving Serially

---
+++ 80+ Problem Solving/src/Problem_01.java
@@ -0,0 +1,9 @@
+public class Problem_01 {
+    
+    public static void PrintInfo() {
+        System.out.println("Name : \"Rajowan\" \nDate of Birth : 01 - 01 - 2000\nMobile Number : 0123456789");
+    }
+    public static void main(String[] args) {
+        PrintInfo();
+    }
+}
