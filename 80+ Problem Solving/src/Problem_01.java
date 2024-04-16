24336a8f8d27b1efc61e4b371c5453302c8b1446
Rajowan <110572622+RajowanRJ@users.noreply.github.com>
2024-04-16 18:27:22 +0600

    Delete 80+ Problem Solving directory

--- 80+ Problem Solving/src/Problem_01.java
+++
@@ -1,9 +0,0 @@
-public class Problem_01 {
-    
-    public static void PrintInfo() {
-        System.out.println("Name : \"Rajowan\" \nDate of Birth : 01 - 01 - 2000\nMobile Number : 0123456789");
-    }
-    public static void main(String[] args) {
-        PrintInfo();
-    }
-}
